b19f5a164329d8bf8251fe24f67f4319d63371c3
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-05-06 10:42:23 +0200

    wip

--- src/librustc_incremental/persist/dirty_clean.rs
+++ src/librustc_incremental/persist/dirty_clean.rs
@@ -15,7 +15,7 @@
 
 use std::iter::FromIterator;
 use std::vec::Vec;
-use rustc::dep_graph::{DepNode, label_strs};
+use rustc::dep_graph::{CompletedDepGraph, ReconstructedDepGraph, DepNode, label_strs};
 use rustc::hir;
 use rustc::hir::{ItemKind as HirItem, ImplItemKind, TraitItemKind};
 use rustc::hir::Node as HirNode;
@@ -206,16 +206,21 @@ impl Assertion {
     }
 }
 
-pub fn check_dirty_clean_annotations<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>) {
+pub fn check_dirty_clean_annotations<'a, 'tcx>(
+    tcx: TyCtxt<'a, 'tcx, 'tcx>,
+    dep_graph: &CompletedDepGraph,
+) {
     // can't add `#[rustc_dirty]` etc without opting in to this feature
     if !tcx.features().rustc_attrs {
         return;
     }
 
     tcx.dep_graph.with_ignore(|| {
+        let graph = ReconstructedDepGraph::new(dep_graph);
         let krate = tcx.hir().krate();
         let mut dirty_clean_visitor = DirtyCleanVisitor {
             tcx,
+            graph,
             checked_attrs: Default::default(),
         };
         krate.visit_all_item_likes(&mut dirty_clean_visitor);
@@ -236,6 +241,7 @@ pub fn check_dirty_clean_annotations<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>) {
 
 struct DirtyCleanVisitor<'a, 'tcx:'a> {
     tcx: TyCtxt<'a, 'tcx, 'tcx>,
+    graph: ReconstructedDepGraph,
     checked_attrs: FxHashSet<ast::AttrId>,
 }
 
@@ -460,7 +466,7 @@ impl<'a, 'tcx> DirtyCleanVisitor<'a, 'tcx> {
             })
     }
 
-    fn _dep_node_str(&self, dep_node: &DepNode) -> String {
+    fn dep_node_str(&self, dep_node: &DepNode) -> String {
         if let Some(def_id) = dep_node.extract_def_id(self.tcx) {
             format!("{:?}({})",
                     dep_node.kind,
@@ -470,11 +476,10 @@ impl<'a, 'tcx> DirtyCleanVisitor<'a, 'tcx> {
         }
     }
 
-    fn assert_dirty(&self, _item_span: Span, dep_node: DepNode) {
+    fn assert_dirty(&self, item_span: Span, dep_node: DepNode) {
         debug!("assert_dirty({:?})", dep_node);
-        panic!()
-        /*let dep_node_index = self.tcx.dep_graph.dep_node_index_of(&dep_node);
-        let current_fingerprint = self.tcx.dep_graph.fingerprint_of(dep_node_index);
+        let dep_node_index = self.graph.dep_node_index_of(&dep_node);
+        let current_fingerprint = self.graph.fingerprint_of(dep_node_index);
         let prev_fingerprint = self.tcx.dep_graph.prev_fingerprint_of(&dep_node);
 
         if Some(current_fingerprint) == prev_fingerprint {
@@ -482,14 +487,13 @@ impl<'a, 'tcx> DirtyCleanVisitor<'a, 'tcx> {
             self.tcx.sess.span_err(
                 item_span,
                 &format!("`{}` should be dirty but is not", dep_node_str));
-        }*/
+        }
     }
 
-    fn assert_clean(&self, _item_span: Span, dep_node: DepNode) {
+    fn assert_clean(&self, item_span: Span, dep_node: DepNode) {
         debug!("assert_clean({:?})", dep_node);
-        panic!()
-        /*let dep_node_index = self.tcx.dep_graph.dep_node_index_of(&dep_node);
-        let current_fingerprint = self.tcx.dep_graph.fingerprint_of(dep_node_index);
+        let dep_node_index = self.graph.dep_node_index_of(&dep_node);
+        let current_fingerprint = self.graph.fingerprint_of(dep_node_index);
         let prev_fingerprint = self.tcx.dep_graph.prev_fingerprint_of(&dep_node);
 
         if Some(current_fingerprint) != prev_fingerprint {
@@ -497,7 +501,7 @@ impl<'a, 'tcx> DirtyCleanVisitor<'a, 'tcx> {
             self.tcx.sess.span_err(
                 item_span,
                 &format!("`{}` should be clean but is not", dep_node_str));
-        }*/
+        }
     }
 
     fn check_item(&mut self, item_id: hir::HirId, item_span: Span) {
